b46787213d82e9af839eb20d04c6c9fb088dfa82
Munashe Chapata <munachaps@gmail.com>
2024-01-05 13:31:29 +0200

    Created header and footer layouts

---
+++ FoodBord/resources/views/layouts/header.blade.php
@@ -0,0 +1,44 @@
+<a href="#menu" class="btn-mobile">
+    <div class="hamburger hamburger--spin" id="hamburger">
+        <div class="hamburger-box">
+            <div class="hamburger-inner"></div>
+        </div>
+    </div>
+</a>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-3 col-6">
+            <div id="logo">
+                <h1><a href="{{route('index')}}" title="FoodBoard">FoodBoard</a></h1>
+            </div>
+        </div>
+        <div class="col-lg-9 col-6">
+            <ul id="menuIcons">
+                <li><a href="#"><i class="icon icon-support"></i></a></li>
+                <li><a href="#"><i class="icon icon-shopping-cart2"></i></a></li>
+            </ul>
+            <!-- Menu -->
+            <nav id="menu" class="main-menu">
+                <ul>
+                    <li><span><a href="{{route('index')}}">Home</a></span></li>
+                    <li>
+                        <span><a href="#">Order <i class="fa fa-chevron-down"></i></a></span>
+                        <ul>
+                            <li>
+                                <span><a href="">Pay online</a></span>
+                                
+                            </li>
+                            <li>
+                                <span><a href="#">Pay with cash</a></span>
+                                
+                            </li>
+                        </ul>
+                    </li>
+                    <li><span><a href="">Login</a></span></li>
+                    <li><span><a href="">Register</a></span></li>
+                </ul>
+            </nav>
+            <!-- Menu End -->
+        </div>
+    </div>
+</div>
--- FoodBord/resources/views/pay-with-cash/cash-payment.blade.php
+++ FoodBord/resources/views/pay-with-cash/cash-payment.blade.php
@@ -7,7 +7,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="description" content="Food order wizard with online payment">
-	<meta name="author" content="UWS">
+	<meta name="author" content="">
 	<title>FoodBoard | Food delivery dashboard</title>
 
 	<!-- Favicon -->
@@ -48,56 +48,7 @@
 
 		<!-- Header -->
 		<header class="main-header sticky">
-			<a href="#menu" class="btn-mobile">
-				<div class="hamburger hamburger--spin" id="hamburger">
-					<div class="hamburger-box">
-						<div class="hamburger-inner"></div>
-					</div>
-				</div>
-			</a>
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-3 col-6">
-						<div id="logo">
-							<h1><a href="{{route('index')}}" title="FoodBoard">FoodBoard</a></h1>
-						</div>
-					</div>
-					<div class="col-lg-9 col-6">
-						<ul id="menuIcons">
-							<li><a href="#"><i class="icon icon-support"></i></a></li>
-							<li><a href="#"><i class="icon icon-shopping-cart2"></i></a></li>
-						</ul>
-						<!-- Menu -->
-						<nav id="menu" class="main-menu">
-							<ul>
-								<li><span><a href="{{route('index')}}">Home</a></span></li>
-								<li>
-									<span><a href="#">Order <i class="fa fa-chevron-down"></i></a></span>
-									<ul>
-										<li>
-											<span><a href="#">Pay online</a></span>
-											<ul>
-												<li><a href="../pay-with-card-online/">Demo 1 - Filtering</a></li>
-												<li><a href="../pay-with-card-online/order-2.php">Demo 2 - Sticky navigation</a></li>
-											</ul>
-										</li>
-										<li>
-											<span><a href="#">Pay with cash</a></span>
-											<ul>
-												<li><a href="../pay-with-cash-on-delivery/">Demo 1 - Filtering</a></li>
-												<li><a href="../pay-with-cash-on-delivery/order-2.php">Demo 2 - Sticky navigation</a></li>
-											</ul>
-										</li>
-									</ul>
-								</li>
-								<li><span><a href="../faq.html">Faq</a></span></li>
-								<li><span><a href="../contacts.html">Contacts</a></span></li>
-							</ul>
-						</nav>
-						<!-- Menu End -->
-					</div>
-				</div>
-			</div>
+		   @include('layouts.header')
 		</header>
 		<!-- Header End -->
 
@@ -605,58 +556,8 @@
 		<!-- Main End -->
 
 		<!-- Footer -->
-		<footer class="main-footer">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-3">
-						<h5 class="footer-heading">Menu Links</h5>
-						<ul class="list-unstyled nav-links">
-							<li><i class="fa fa-angle-right"></i> <a href="https://ultimatewebsolutions.net/foodboard/" class="footer-link">Home</a></li>
-							<li><i class="fa fa-angle-right"></i> <a href="../faq.html" class="footer-link">FAQ</a></li>
-							<li><i class="fa fa-angle-right"></i> <a href="../contacts.html" class="footer-link">Contacts</a></li>
-						</ul>
-					</div>
-					<div class="col-md-3">
-						<h5 class="footer-heading">Order Wizard</h5>
-						<ul class="list-unstyled nav-links">
-							<li><i class="fa fa-angle-right"></i> <a href="../pay-with-card-online/" class="footer-link">Pay online</a></li>
-							<li><i class="fa fa-angle-right"></i> <a href="../pay-with-cash-on-delivery/" class="footer-link">Pay with cash on delivery</a></li>
-						</ul>
-					</div>
-					<div class="col-md-4">
-						<h5 class="footer-heading">Contacts</h5>
-						<ul class="list-unstyled contact-links">
-							<li><i class="icon icon-map-marker"></i><a href="https://goo.gl/maps/vKgGyZe2JSRLDnYH6" class="footer-link" target="_blank">Address: 1234 Street Name, City Name, USA</a>
-							</li>
-							<li><i class="icon icon-envelope3"></i><a href="mailto:info@yourdomain.com" class="footer-link">Mail: info@yourdomain.com</a></li>
-							<li><i class="icon icon-phone2"></i><a href="tel:+3630123456789" class="footer-link">Phone: +3630123456789</a></li>
-						</ul>
-					</div>
-					<div class="col-md-2">
-						<h5 class="footer-heading">Find Us On</h5>
-						<ul class="list-unstyled social-links">
-							<li><a href="https://facebook.com" class="social-link" target="_blank"><i class="icon icon-facebook"></i></a></li>
-							<li><a href="https://twitter.com" class="social-link" target="_blank"><i class="icon icon-twitter"></i></a></li>
-							<li><a href="https://instagram.com" class="social-link" target="_blank"><i class="icon icon-instagram"></i></a></li>
-							<li><a href="https://pinterest.com" class="social-link" target="_blank"><i class="icon icon-pinterest"></i></a></li>
-						</ul>
-					</div>
-				</div>
-				<hr>
-				<div class="row">
-					<div class="col-md-8">
-						<ul id="subFooterLinks">
-							<li><a href="https://themeforest.net/user/ultimatewebsolutions" target="_blank">With <i class="fa fa-heart pulse"></i> by UWS</a></li>
-							<li><a href="../pdf/terms.pdf" target="_blank">Terms and conditions</a></li>
-						</ul>
-					</div>
-					<div class="col-md-4">
-						<div id="copy">© 2021 FoodBoard</div>
-					</div>
-				</div>
-			</div>
-		</footer>
-		<!-- Footer End -->
+            @include('layouts.footer')
+        <!-- Footer End -->
 
 		<!-- Notification Messages -->
 		<div class="addedToCartMsg">Added to cart</div>
